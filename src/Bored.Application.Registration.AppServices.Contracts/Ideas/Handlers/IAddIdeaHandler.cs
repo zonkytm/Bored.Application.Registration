b111938cdbe404bb7c78800d916cdaf5962b859b
zonkytm <juphy@yandex.ru>
2024-04-24 23:55:27 +0300

    Добавил обработку события из кафки для создания идей

---
+++ src/Bored.Application.Registration.AppServices.Contracts/Ideas/Handlers/IAddIdeaHandler.cs
@@ -0,0 +1,8 @@
+﻿using Bored.Application.Registration.AppServices.Contracts.Ideas.Infos;
+
+namespace Bored.Application.Registration.AppServices.Contracts.Ideas.Handlers;
+
+public interface IAddIdeaHandler
+{
+    Task Handle(AddIdeaInfo addIdeaInfo);
+}
--- src/Bored.Application.Registration.Host/Kafka/Handlers/AcceptIdeaEventHandler.cs
+++ src/Bored.Application.Registration.Host/Kafka/Handlers/AcceptIdeaEventHandler.cs
@@ -1,6 +1,13 @@
-﻿namespace Bored.Application.Registration.Host.Kafka.Handlers;
+﻿using Bored.Application.Registration.AppServices.Contracts.Users.Handlers;
+
+namespace Bored.Application.Registration.Host.Kafka.Handlers;
 
 public class AcceptIdeaEventHandler
 {
-    
+    private readonly IAddUserHandler _addUserHandler;
+
+    public AcceptIdeaEventHandler(IAddUserHandler addUserHandler)
+    {
+        _addUserHandler = addUserHandler;
+    }
 }
